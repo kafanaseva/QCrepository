98b159daf2433dac8e163ae76c17da2ea29aded4
KAfanaseva <k.afanaseva@room8studio.com>
2021-04-14 16:45:28 +0300

    test project init commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    public float speed = 0.1f;
+
+    private void Update()
+    {
+        transform.position += new Vector3(speed, 0, 0) * Input.GetAxis("Horizontal");
+        transform.position += new Vector3(0, speed, 0) * Input.GetAxis("Vertical");
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
